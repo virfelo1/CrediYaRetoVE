9273820ef0ca32dd885739b4957a7b11a81af61a
vescobar <vescobar@bancolombia.com.co>
2025-08-30 16:14:40 -0500

    Se agragan configuraciones de seguridad con Spring security

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/projectve/api/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package co.com.projectve.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(authorize -> authorize
+                        .anyExchange().authenticated()
+                        //.permitAll()
+
+                )
+                .httpBasic(httpBasic -> {})
+                .build();
+    }
+}
